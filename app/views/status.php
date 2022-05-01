e132b11c2fee871fd95a228fce7680999109b1e7
maks706 <66426962+maks706@users.noreply.github.com>
2022-05-01 15:44:40 +0600

    Update status.php

--- app/views/status.php
+++ app/views/status.php
@@ -14,19 +14,18 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Басты <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="page_login.html">Войти</a>
+                    <a class="nav-link" href="/login">Кіру</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Выйти</a>
+                    <a class="nav-link" href="/logout">Шығу</a>
                 </li>
             </ul>
         </div>
@@ -34,7 +33,7 @@
     <main id="js-page-content" role="main" class="page-content mt-3">
         <div class="subheader">
             <h1 class="subheader-title">
-                <i class='subheader-icon fal fa-sun'></i> Установить статус
+                <i class='subheader-icon fal fa-sun'></i> Статус орнату
             </h1>
 
         </div>
@@ -44,23 +43,23 @@
                     <div id="panel-1" class="panel">
                         <div class="panel-container">
                             <div class="panel-hdr">
-                                <h2>Установка текущего статуса</h2>
+                                <h2>Қазіргі статусты орнату</h2>
                             </div>
                             <div class="panel-content">
                                 <div class="row">
                                     <div class="col-md-4">
                                         <!-- status -->
                                         <div class="form-group">
-                                            <label class="form-label" for="example-select">Выберите статус</label>
+                                            <label class="form-label" for="example-select">Статусты таңдаңыз</label>
                                             <select class="form-control" id="example-select" name="status">
                                                 <option value="Online" <?php if($status=='Online'){echo 'selected';}?>>Online</option>
-                                                <option value="Otoshel" <?php if($status=='Otoshel'){echo 'selected';}?>>Otoshel</option>
-                                                <option value="Ne bespokoit"<?php if($status=='Ne bespokoit'){echo 'selected';}?> >Не беспокоить</option>
+                                                <option value="Departed" <?php if($status=='Departed'){echo 'selected';}?>>Кетті</option>
+                                                <option value="Do not disturb"<?php if($status=='Do not disturb'){echo 'selected';}?> >Мазаламау</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div class="col-md-12 mt-3 d-flex flex-row-reverse">
-                                        <button class="btn btn-warning">Set Status</button>
+                                        <button class="btn btn-warning">Статусты орнату</button>
                                     </div>
                                 </div>
                             </div>
