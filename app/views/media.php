4ccebcbd4ff5c93871ec95fd7816ca0f390dcdf3
maks706 <66426962+maks706@users.noreply.github.com>
2022-05-01 14:38:20 +0600

    Update media.php

--- app/views/media.php
+++ app/views/media.php
@@ -14,19 +14,18 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Басты <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Войти</a>
+                    <a class="nav-link" href="/login">Кіру</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/logout">Выйти</a>
+                    <a class="nav-link" href="/logout">Шығу</a>
                 </li>
             </ul>
         </div>
@@ -34,7 +33,7 @@
     <main id="js-page-content" role="main" class="page-content mt-3">
         <div class="subheader">
             <h1 class="subheader-title">
-                <i class='subheader-icon fal fa-image'></i> Загрузить аватар
+                <i class='subheader-icon fal fa-image'></i> Аватар жүктеу
             </h1>
 
         </div>
@@ -44,7 +43,7 @@
                     <div id="panel-1" class="panel">
                         <div class="panel-container">
                             <div class="panel-hdr">
-                                <h2>Текущий аватар</h2>
+                                <h2>Қазіргі аватар</h2>
                             </div>
                             <div class="panel-content">
                                 <div class="form-group">
@@ -52,13 +51,13 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <label class="form-label" for="example-fileinput">Выберите аватар</label>
+                                    <label class="form-label" for="example-fileinput">Аватарды таңдаңыз</label>
                                     <input type="file" id="example-fileinput" class="form-control-file" name="avatar">
                                 </div>
 
 
                                 <div class="col-md-12 mt-3 d-flex flex-row-reverse">
-                                    <button class="btn btn-warning">Загрузить</button>
+                                    <button class="btn btn-warning">Жүктеу</button>
                                 </div>
                             </div>
                         </div>
