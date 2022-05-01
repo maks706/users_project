74479b2cfd3403323e06de089faffd7417ca339f
maks706 <66426962+maks706@users.noreply.github.com>
2022-05-01 14:27:15 +0600

    Update edit.php

--- app/views/edit.php
+++ app/views/edit.php
@@ -14,19 +14,18 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Басты <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Войти</a>
+                    <a class="nav-link" href="/login">Кіру</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/logout">Выйти</a>
+                    <a class="nav-link" href="/logout">Шығу</a>
                 </li>
             </ul>
         </div>
@@ -34,7 +33,7 @@
     <main id="js-page-content" role="main" class="page-content mt-3">
         <div class="subheader">
             <h1 class="subheader-title">
-                <i class='subheader-icon fal fa-plus-circle'></i> Редактировать
+                <i class='subheader-icon fal fa-plus-circle'></i>Өңдеу
             </h1>
 
         </div>
@@ -44,34 +43,34 @@
                     <div id="panel-1" class="panel">
                         <div class="panel-container">
                             <div class="panel-hdr">
-                                <h2>Общая информация</h2>
+                                <h2>Жалпы ақпарат</h2>
                             </div>
                             <div class="panel-content">
                                 <!-- username -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Имя</label>
+                                    <label class="form-label" for="simpleinput">Есімі</label>
                                     <input type="text" id="simpleinput" class="form-control" name="username" value="<?php echo $username;?>">
                                 </div>
 
                                 <!-- title -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Место работы</label>
+                                    <label class="form-label" for="simpleinput">Жұмыс орны</label>
                                     <input type="text" id="simpleinput" class="form-control" name="speciality" value="<?php echo $speciality;?>">
                                 </div>
 
                                 <!-- tel -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Номер телефона</label>
+                                    <label class="form-label" for="simpleinput">Телефон нөмірі</label>
                                     <input type="text" id="simpleinput" class="form-control" name="telnumber" value="<?php echo $telnumber;?>">
                                 </div>
 
                                 <!-- address -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Адрес</label>
+                                    <label class="form-label" for="simpleinput">Мекен-жайы</label>
                                     <input type="text" id="simpleinput" class="form-control" name="address" value="<?php echo $address;?>">
                                 </div>
                                 <div class="col-md-12 mt-3 d-flex flex-row-reverse">
-                                    <button class="btn btn-warning">Редактировать</button>
+                                    <button class="btn btn-warning">Өңдеу</button>
                                 </div>
                             </div>
                         </div>
