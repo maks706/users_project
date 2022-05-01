c56910119d8e58e0131fe52897ceadd481ccb241
maks706 <66426962+maks706@users.noreply.github.com>
2022-05-01 14:51:45 +0600

    Update security.php

--- app/views/security.php
+++ app/views/security.php
@@ -14,19 +14,18 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-        <a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Басты <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Войти</a>
+                    <a class="nav-link" href="/login">Кіру</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/logout">Выйти</a>
+                    <a class="nav-link" href="/logout">Шығу</a>
                 </li>
             </ul>
         </div>
@@ -34,7 +33,7 @@
     <main id="js-page-content" role="main" class="page-content mt-3">
         <div class="subheader">
             <h1 class="subheader-title">
-                <i class='subheader-icon fal fa-lock'></i> Безопасность
+                <i class='subheader-icon fal fa-lock'></i> Қауіпсіздік
             </h1>
 
         </div>
@@ -44,7 +43,7 @@
                     <div id="panel-1" class="panel">
                         <div class="panel-container">
                             <div class="panel-hdr">
-                                <h2>Обновление эл. адреса и пароля</h2>
+                                <h2>эл. адрес пен құпия сөзді жаңарту </h2>
                             </div>
                             <div class="panel-content">
                                 <!-- email -->
@@ -55,19 +54,19 @@
 
                                 <!-- password -->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Пароль</label>
+                                    <label class="form-label" for="simpleinput">Құрия сөз</label>
                                     <input type="password" id="simpleinput" class="form-control" name="newPassword">
                                 </div>
 
                                 <!-- password confirmation-->
                                 <div class="form-group">
-                                    <label class="form-label" for="simpleinput">Подтверждение пароля</label>
+                                    <label class="form-label" for="simpleinput">Құпич сөзді растау</label>
                                     <input type="password" id="simpleinput" class="form-control" name="newPassword2">
                                 </div>
 
 
                                 <div class="col-md-12 mt-3 d-flex flex-row-reverse">
-                                    <button class="btn btn-warning">Изменить</button>
+                                    <button class="btn btn-warning">Өзгерту</button>
                                 </div>
                             </div>
                         </div>
