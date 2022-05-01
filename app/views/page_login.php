81aec1cd97fe2f56fa26fa06c3a08bb54fedd07f
maks706 <66426962+maks706@users.noreply.github.com>
2022-05-01 14:41:13 +0600

    Update page_login.php

--- app/views/page_login.php
+++ app/views/page_login.php
@@ -26,11 +26,7 @@
 <body>
     <div class="blankpage-form-field">
         <div class="page-logo m-0 w-100 align-items-center justify-content-center rounded border-bottom-left-radius-0 border-bottom-right-radius-0 px-4">
-            <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
-                <img src="img/logo.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
-                <span class="page-logo-text mr-1">Учебный проект</span>
-                <i class="fal fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
-            </a>
+            
         </div>
         <div class="card p-4 border-top-left-radius-0 border-top-right-radius-0">
             <?= flash()->display();?>
@@ -40,20 +36,20 @@
                     <input type="email" name="email" id="username" class="form-control" placeholder="Эл. адрес" value="">
                 </div>
                 <div class="form-group">
-                    <label class="form-label" for="password">Пароль</label>
+                    <label class="form-label" for="password">Құпия сөз</label>
                     <input type="password" name="password" id="password" class="form-control" placeholder="" >
                 </div>
                 <div class="form-group text-left">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="rememberme">
-                        <label class="custom-control-label" for="rememberme">Запомнить меня</label>
+                        <label class="custom-control-label" for="rememberme">Мені есте сақтау</label>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-default float-right">Войти</button>
+                <button type="submit" class="btn btn-default float-right">Кіру</button>
             </form>
         </div>
         <div class="blankpage-footer text-center">
-            Нет аккаунта? <a href="/"><strong>Зарегистрироваться</strong>
+            Нет аккаунта? <a href="/"><strong>Тіркелу</strong>
         </div>
     </div>
     <video poster="img/backgrounds/clouds.png" id="bgvid" playsinline autoplay muted loop>
